69ba5ec941f330b9404594635bcf4068812de379
josuelopez <tshadez@hotmail.com>
2023-11-07 21:49:03 -0500

    feat:adding new component\view

--- components/views/card.py
+++ components/views/card.py
@@ -6,22 +6,30 @@ class Card:
         self._card_body = kwargs.get("card_body",'Body card')
         self._card_icon = kwargs.get("card_icon",None)
         self._card_icon_color = kwargs.get("card_icon_color",None)
-        self._card_route  = kwargs.get("route_view",None)
+        self._card_route  = kwargs.get("card_route",None)
         self._card_disabled = kwargs.get("card_disabled", False)
 
     def build(self) -> ft.Card:
         return ft.Card(
-            content=ft.ListTile(
-                leading=self._icon_card(),
-                title=self._title_card(),
-                subtitle=self._subtitle_card(),
-                on_click=self._card_route,
-            ),
             disabled=self._card_disabled,
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.ListTile(
+                            width=420,
+                            height=200,
+                            leading=self._icon_card(),
+                            title=self._title_card(),
+                            subtitle=self._subtitle_card(),
+                            on_click=self._card_route,
+                        ),
+                    ]
+                )
+            )
         )
     
     def _icon_card(self) -> ft.Icon:
-        return ft.Icon(self._card_icon,size=50,color=self._card_icon_color)
+        return ft.Icon(self._card_icon,size=55,color=self._card_icon_color)
     
     def _title_card(self) -> ft.Text:
         return ft.Text(self._card_title,size=15,weight=ft.FontWeight.W_300)
