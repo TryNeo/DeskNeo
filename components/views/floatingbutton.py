1a3e22835174d371f3df8d8385f69bec5eecbe6d
josuelopez <tshadez@hotmail.com>
2023-11-08 21:18:03 -0500

    feat:add new component view floatingButton

---
+++ components/views/floatingbutton.py
@@ -0,0 +1,15 @@
+import flet as ft
+
+class FloatingButton:
+    def __init__(self, page:ft.Page,route_url:str):
+        self._route_url = route_url
+        self.page = page
+
+    def build(self) -> ft.FloatingActionButton:
+        return ft.FloatingActionButton(
+            icon=ft.icons.SUBDIRECTORY_ARROW_LEFT,
+            on_click=lambda _: self.page.go(self._route_url),
+            tooltip="Regresar al inicio",
+            mini=True,
+            bgcolor=ft.colors.SURFACE,
+        )
